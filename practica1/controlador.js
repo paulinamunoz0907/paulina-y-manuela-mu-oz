e8707dfe9554b902dc60b41c97cb261ed39c2dc0
paulinamunoz0907 <71206032+paulinamunoz0907@users.noreply.github.com>
2021-07-21 16:51:53 -0500

    Initial commit

---
+++ practica1/controlador.js
@@ -0,0 +1,6 @@
+//programa que bnos ayude a establecer el costo total de una consola de videojuegos
+
+
+//en el exterior
+//compra en dolares
+//pagar un casillero (dolares costo dewpendoiendo de la cantidad de productos)
